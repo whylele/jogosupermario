1c804b535d0fa1d5aec56522cf032f77a0ec57da
whylele <163873282+whylele@users.noreply.github.com>
2024-08-26 10:34:52 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
+const jump = () => (
+    mario.classList.add("jump");
+
+    setTimeout(() =>{
+      mario.classList.remove("jump");
+    }
+,500);
+}
+const loop = setInterval(() => {});
