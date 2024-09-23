0311354d068b15d62bb9d5bc203f9d94fc1880c6
whylele <163873282+whylele@users.noreply.github.com>
2024-09-23 11:05:59 -0300

    .

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ const pipe = document.querySelector(".pipe");
 const nuvem = document.querySelector(".nuvem");
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
+const scoreElement = document.querySelector(".score");
 
 audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
 gameStarded = true;
 audioStart.play();
+
 pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
     startButton.style.display = "none";
@@ -19,8 +22,7 @@ pipe.style.animation = "pipe-animation 1.5s infinite linear";
     pipe.style.opacity = "1";
     nuvem.style.opacity = "1";
 
-}
-
+  }
 
 const jump = () => {
     if(gameStarded){
@@ -31,12 +33,17 @@ const jump = () => {
     }
     ,500 );
     }
+    }
 
+    const updateScore = () => {
+        score += 1;
+        scoreElement.textContent = score;
+      }
     const loop = setInterval (() => {
      const pipePosition = pipe.offsetLeft;
      const marioPosition = window.getComputedStyle(mario).bottom.replace("px","");
 
-     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
 
@@ -50,15 +57,19 @@ const jump = () => {
 
         gameOverSound.play();
 
-    
-     }
-     clearInterval(loop);
-      
+        clearInterval(loop);
+        gameOverScreen.style.display = "flex";
+      }  else if (pipePosition < 0 && gameStarted) { 
+        updateScore();
+        pipe.style.left = '';
+      }
     },10);
-}
+    
     document.addEventListener("keydown", jump);
-
+    
     const restartGame = () => {
-
-        window.location.reload();
+    
+      window.location.reload();
     }
+    
+     
