d82e15c67cc5c30c898b2ae740dd9c1ea4e4773b
whylele <163873282+whylele@users.noreply.github.com>
2024-09-24 13:13:47 -0300

    .

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.querySelector(".score");
 
-audioStart = new Audio("./sound/audio_theme.mp3");
+audioStart = new Audio("./sound/hello-kitty's-paradise-made-with-Voicemod.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
